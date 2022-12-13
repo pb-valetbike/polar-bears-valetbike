41df7aa3490df948c915bc5692c9ce28f9a1e817
Liam <ldillondavidson@smith.edu>
2022-12-12 21:14:19 -0500

    Updated pricing page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_11_010721) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_141004) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_11_010721) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "points"
   end
 
 end
